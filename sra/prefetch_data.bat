3bc5586dcf349bc86a40c61b3b66b364936c227a
Antonio Carvalho <anthony.durden@tutamail.com>
2024-05-22 13:45:00 +0100

    Update

---
+++ sra/prefetch_data.bat
@@ -0,0 +1,20 @@
+@echo off
+
+echo Prefetching...
+
+echo.
+echo E. Coli:
+prefetch SRR29080237
+
+echo.
+echo Smallpox:
+prefetch ERR4333272
+
+echo.
+echo Polio:
+prefetch SRR25182350
+
+echo.
+echo Rubella:
+prefetch SRR26754123
+
---
+++ sra/prefetch_data.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Prefetching..."
+
+printf "\nE. Coli:\n"
+prefetch SRR29080237
+
+printf "\nSmallpox:\n"
+prefetch ERR4333272
+
+printf "\nPolio:\n"
+prefetch SRR25182350
+
+printf "\nRubella:\n"
+prefetch SRR26754123
