560e07d06e0ef6c7ee4ae3572289aa27baa0618b
Antonio Carvalho <anthony.durden@tutamail.com>
2024-05-22 20:52:17 +0100

    Added fastq_dump

--- sra/fasterq_dump.sh
+++ sra/fasterq_dump.sh
@@ -1,1 +1,22 @@
- 
+#!/bin/sh
+
+OUTPUT_DIR="./fastq"
+
+if [ ! -d $OUTPUT_DIR ]; then
+    mkdir $OUTPUT_DIR
+fi
+
+echo "Dumping fastq..."
+
+printf "\nE. Coli:\n"
+fasterq-dump SRR29080237 -O $OUTPUT_DIR
+
+printf "\nSmallpox:\n"
+fasterq-dump ERR4333272 -O $OUTPUT_DIR
+
+printf "\nPolio:\n"
+fasterq-dump SRR25182350 -O $OUTPUT_DIR
+
+printf "\nRubella:\n"
+fasterq-dump SRR26754123 -O $OUTPUT_DIR
+
